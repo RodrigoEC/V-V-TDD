cba095376c7351d0d7840dfc2cbebc76ddb1bb1c
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-03-06 19:59:31 -0300

    Adds TDD iteration on Pagamento's attributes

--- ProcessadorDeBoletos/src/main/java/org/example/Pagamento.java
+++ ProcessadorDeBoletos/src/main/java/org/example/Pagamento.java
@@ -3,7 +3,26 @@ package org.example;
 import java.util.Date;
 
 public class Pagamento {
-    public Pagamento(double valuePaid, Date date, String type) {
+    private final String type;
+    private final Date date;
+    private final double valuePaid;
 
+    public Pagamento(double valuePaid, Date date, String type) {
+        this.valuePaid = valuePaid;
+        this.date = date;
+        this.type = type;
+
+    }
+
+    public double getValuePaid() {
+        return this.valuePaid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Date getDate() {
+        return date;
     }
 }
--- ProcessadorDeBoletos/src/test/java/org/example/PagamentoTest.java
+++ ProcessadorDeBoletos/src/test/java/org/example/PagamentoTest.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Array;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class PagamentoTest {
 
@@ -14,10 +15,28 @@ public class PagamentoTest {
 
     String type = "BOLETO";
 
-
     @Test
     public void testCreatePagamento() {
         Pagamento pagamento = new Pagamento(this.valuePaid, this.date, this.type);
         assertEquals(pagamento.getClass(), Pagamento.class);
     }
+
+    @Test
+    public void testGetPagamentoValuePaid() {
+        Pagamento pagamento = new Pagamento(this.valuePaid, this.date, this.type);
+        assertEquals(pagamento.getValuePaid(), this.valuePaid, 0);
+    }
+
+    @Test
+    public void testGetPagamentoDate() {
+        Pagamento pagamento = new Pagamento(this.valuePaid, this.date, this.type);
+        assertEquals(pagamento.getDate(), this.date);
+    }
+
+    @Test
+    public void testGetPagamentoType() {
+        Pagamento pagamento = new Pagamento(this.valuePaid, this.date, this.type);
+        assertEquals(pagamento.getType(), this.type);
+    }
+    
 }
