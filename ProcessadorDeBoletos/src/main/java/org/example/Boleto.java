25dbb739db599e5d877f498627d7c9489546e5ed
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-03-06 19:03:18 -0300

    Adds first TDD iteration, creating Boleto

---
+++ ProcessadorDeBoletos/src/main/java/org/example/Boleto.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Date;
+
+public class Boleto {
+
+    public Boleto(String codigo, Date data, float valor) {
+    }
+
+}
--- ProcessadorDeBoletos/src/test/java/org/example/BoletoTest.java
+++ ProcessadorDeBoletos/src/test/java/org/example/BoletoTest.java
@@ -1,6 +1,17 @@
 package org.example;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class BoletoTest {
+
+    @Test
+    public void testCreateBoleto() {
+        Boleto boleto = new Boleto("123124125412", new Date(), 50);
+        assertEquals(boleto.getClass(), Boleto.class);
+    }
 }
