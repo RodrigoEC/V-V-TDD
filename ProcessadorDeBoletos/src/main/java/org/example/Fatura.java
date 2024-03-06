3f0a08f4c827a6bedcab974a38be4fd7efdedb98
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-03-06 19:34:58 -0300

    Adds new TDD iteration over Fatura's attributes creation

--- ProcessadorDeBoletos/src/main/java/org/example/Fatura.java
+++ ProcessadorDeBoletos/src/main/java/org/example/Fatura.java
@@ -4,6 +4,25 @@ import java.util.Date;
 
 public class Fatura {
 
+    private final String client;
+    private final Date date;
+    private final double price;
+
     public Fatura(String client, Date date, double price) {
+        this.client = client;
+        this.date = date;
+        this.price = price;
+    }
+
+    public String getClient() {
+        return this.client;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public double getPrice() {
+        return this.price;
     }
 }
--- ProcessadorDeBoletos/src/test/java/org/example/FaturaTest.java
+++ ProcessadorDeBoletos/src/test/java/org/example/FaturaTest.java
@@ -8,12 +8,34 @@ import static org.junit.Assert.assertEquals;
 
 public class FaturaTest {
 
+    String client = "Rodrigo Eloy Cavalcanti";
+    Date date = new Date();
+    double price = 1500;
+
 
     @Test
     public void testCreateFatura() {
-        Fatura fatura = new Fatura("Rodrigo Eloy Cavalcanti", new Date(), 1500);
+        Fatura fatura = new Fatura(this.client, this.date, this.price);
         assertEquals(fatura.getClass(), Fatura.class);
-
     }
+
+    @Test
+    public void testGetFaturasClient() {
+        Fatura fatura = new Fatura(this.client, this.date, this.price);
+        assertEquals(fatura.getClient(), this.client);
+    }
+
+    @Test
+    public void testGetFaturasDate() {
+        Fatura fatura = new Fatura(this.client, this.date, this.price);
+        assertEquals(fatura.getDate(), this.date);
+    }
+
+    @Test
+    public void testGetFaturasPrice() {
+        Fatura fatura = new Fatura(this.client, this.date, this.price);
+        assertEquals(fatura.getPrice(), this.price, 0);
+    }
+
 
 }
