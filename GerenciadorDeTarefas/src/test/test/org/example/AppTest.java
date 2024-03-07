d5f035369ed4abaaf1083b5d8f376b29fd17c76c
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2024-03-06 23:01:39 -0300

    add setup to GerenciadorDeTarefas project and basic test

---
+++ GerenciadorDeTarefas/src/test/test/org/example/AppTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+        extends TestCase
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
